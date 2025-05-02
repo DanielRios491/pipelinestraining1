629ee0b93b54fe5020b4628031c82d810c4986da
DanielRiosLopez <Daniel.Rios@jala.university>
2025-05-02 19:42:32 -0400

    update validate_report 3

--- scripts/validate_report.ps1
+++ scripts/validate_report.ps1
@@ -1,4 +1,4 @@
-$xmlFilePath = "..\pipelinestraining1\myApp\reports\report.xml"
+$xmlFilePath = "..\pipelinestraining1\myApp\reports\report"
 
 # Read the XML file content
 $xmlContent = Get-Content -Path $xmlFilePath -Raw
